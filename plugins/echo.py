aefd03a3bd1d694951e72fec125ed046fc9eb634
Alireza-shahbaziyan <alireza0831@gmail.com>
2021-09-20 01:59:17 +0430

    Change the start command

--- plugins/echo.py
+++ plugins/echo.py
@@ -1,7 +1,18 @@
 from pyrogram import Client, filters
-
+from pyrogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 
 
 @Client.on_message(filters.command('start'))
 def start(client, message):
-	message.reply_text('Hello there...')
+    chat_id = message.chat.id
+    client.send_message(
+        chat_id,
+        "please choose",
+        reply_markup=ReplyKeyboardMarkup(
+            [
+                ["download  from YT", "download from PH"],  # First row
+                ["Communication with admin"]  # second row
+            ],
+            resize_keyboard=True
+        )
+    )
